db01b592834f79dd35c185f84e6f19f2dcc5e8e3
lamineg123 <117523602+lamineg123@users.noreply.github.com>
2024-10-05 22:26:28 -0400

    Update web.php

--- routes/web.php
+++ routes/web.php
@@ -2,13 +2,13 @@
 
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\ProductController;
-use App\HTTP\Controllers\CartController;
+use App\Http\Controllers\CartController;
 
 Route::get('/', function (){
     return view('welcome');
 });
 //should display products
-Route::get('/products', [ProductController::class, 'show']);
+Route::get('/products', [ProductController::class, 'index']);
 //Route to add products to cart
 Route::post('/cart/add', [CartController::class, 'addToCart'])->name('cart.add');
 //route to view the cart
